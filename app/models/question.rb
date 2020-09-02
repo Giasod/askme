3c370782baaa724cc25eea91c40b41f4d52fb9a9
Amiran <amiranlevanovich@gmail.com>
2020-09-02 14:01:31 +0300

    Added validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,6 @@ class Question < ApplicationRecord
   belongs_to :user
   
   validates :text, :user, presence: true
+  validates :text, length: { maximum: 255, too_long: '%{count} characters is the maximum allowed' }
   
 end
