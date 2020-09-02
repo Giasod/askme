25f7025ec9c1ce13ac97f79cccdbc81f98db5d6d
Amiran <amiranlevanovich@gmail.com>
2020-09-02 17:25:21 +0300

    Few changes in the layout, improved validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,82 +1,59 @@
-# frozen_string_literal: true
-
 require 'openssl'
 
 class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
 
-  has_many :questions
-
-  before_validation :username_downcase
-  
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
-  
-  validates :username, length: { maximum: 40, too_long: '%{count} characters is the maximum allowed' }
-  
-  validates :email, format: { with: /[\w+\-\_.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+/i }
-  validates :username, format: { with: /[\w\d\_]{1,40}/i, message: 'You can only use latin letters, digits or _ character'}
-
   attr_accessor :password
 
-  validates_presence_of :password, on: :create
-  validates_confirmation_of :password
+  has_many :questions
 
+  validates :email, :username, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true, on: :create
+
+  validates :username, length: { maximum: 40, too_long: '%{count} characters is the maximum allowed' }
+
+  validates :email, format: { with: /[\-\w+.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+/i }
+  validates :username, format: { with: /\w/i }
+
+  before_validation :username_downcase
   before_save :encrypt_password
 
-  def username_downcase
-    self.username = username.downcase!
-  end
-  
   def encrypt_password
     if password.present?
-      # Создаем т.н. «соль» — случайная строка, усложняющая задачу хакерам по
-      # взлому пароля, даже если у них окажется наша БД.
-      # У каждого юзера своя «соль», это значит, что если подобрать перебором пароль
-      # одного юзера, нельзя разгадать, по какому принципу
-      # зашифрованы пароли остальных пользователей
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
 
-      # Создаем хэш пароля — длинная уникальная строка, из которой невозможно
-      # восстановить исходный пароль. Однако, если правильный пароль у нас есть,
-      # мы легко можем получить такую же строку и сравнить её с той, что в базе.
       self.password_hash = User.hash_to_string(
         OpenSSL::PKCS5.pbkdf2_hmac(
           password, password_salt, ITERATIONS, DIGEST.length, DIGEST
         )
       )
-
-      # Оба поля попадут в базу при сохранении (save).
     end
   end
 
-  # Служебный метод, преобразующий бинарную строку в шестнадцатиричный формат,
-  # для удобства хранения.
   def self.hash_to_string(password_hash)
     password_hash.unpack1('H*')
   end
 
   def self.authenticate(email, password)
-    # Сперва находим кандидата по email
     user = find_by(email: email)
 
-    # Если пользователь не найден, возвращает nil
     return nil unless user.present?
 
-    # Формируем хэш пароля из того, что передали в метод
     hashed_password = User.hash_to_string(
       OpenSSL::PKCS5.pbkdf2_hmac(
         password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
       )
     )
 
-    # Обратите внимание: сравнивается password_hash, а оригинальный пароль так
-    # никогда и не сохраняется нигде. Если пароли совпали, возвращаем
-    # пользователя.
     return user if user.password_hash == hashed_password
 
-    # Иначе, возвращаем nil
     nil
+  end
+
+  private
+
+  def username_downcase
+    self.username = username.downcase!
   end
 end
