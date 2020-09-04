2af95f9817a0e90ba0cd1ba9771dcb74d1a3fec5
Amiran <amiranlevanovich@gmail.com>
2020-09-04 12:08:59 +0300

    Revert "Views refactoring incomplete"
    This reverts commit af69e9e0936fa99763163923b92092df19a3de2b.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,3 @@
 Rails.application.routes.draw do
-  # Это правило говорит: если пользователь заходит по адресу /, направь его в
-  # контроллер users, действие index. Грубо говоря, на главной странице у нас
-  # список юзеров.
-  root 'users#index'
-  
-  # Эти две строчки добавляют ресурсы для пользователей и вопросов. Ресурс — это
-  # набор путей для управления соответствующей моделью. Посмотрите, что напишет
-  # команда
-  #
-  # rake routes
-  resources :users
-  resources :questions
-  
-  get 'users/index'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
   get 'show' => 'users#show'
 end
