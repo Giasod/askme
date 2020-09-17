cce43b59ebd2c925b5d5ffa17a7976b04c956742
Amiran <amiranlevanovich@gmail.com>
2020-09-17 16:09:43 +0300

    Added hashtags

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @tags = Tag.with_questions
   end
 
   # Действие new будет отзываться по адресу /users/new
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,20 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
+  has_many :tag_questions, dependent: :destroy
+  has_many :tags, through: :tag_questions
+  
   validates :text, length: { maximum: 255 },
                    presence: true
+  
+  after_commit :update_tags, on: %i[create update]
+  
+  private
+  
+  def update_tags
+    tag_questions.clear
+    "#{text} #{answer}".downcase.scan(Tag::TAG_REGEX).uniq.each do |tag|
+      tags << Tag.find_or_create_by(name: tag)
+    end
+  end
 end
